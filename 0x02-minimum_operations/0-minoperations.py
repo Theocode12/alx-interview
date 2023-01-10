f4914ce7b0078a9e86fb01ab38f9f8d1c2dbe556
Theocode12 <vchidubem9@gmail.com>
2023-01-10 20:23:51 +0100

    reducing space complexity

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,43 +4,22 @@
 
 def minOperations(n: int) -> int:
     """Minimum number of operations needed to get n H characters"""
-    text = 'H'
-    special_num = {2, 3}
 
     if n <= 1:
         return 0
 
-    cp = copy(text)
-    text = paste(cp, text)
+    len_text = 2
+    prev = 1
     op = 2
 
     while True:
-        if n % len(text) == 0 and len(text) != n:
-            cp = copy(text)
-            text = paste(cp, text)
+        if n % len_text == 0 and len_text != n:
+            prev = len_text
+            len_text = len_text * 2
             op += 2
-        elif len(text) == n:
+        elif len_text == n:
             break
         else:
-            text = paste(cp, text)
+            len_text += prev
             op += 1
     return op
-
-
-def copy(text: str) -> str:
-    cp = text[:]
-    return cp
-
-
-def paste(cp: str, text: str) -> str:
-    text = text + cp
-    return text
-
-
-def isPrime(N):
-    count = 2
-    while count ** 2 <= N:
-        if N % count == 0:
-            return False
-        count = count + 1
-    return True
